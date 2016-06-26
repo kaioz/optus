0b191358c700134e412384a277cf1fc2c8bc50de
soar <soarcn@gmail.com>
2016-06-26 22:03:53 +1000

    Mock data

--- app/src/main/java/com/cocosw/optus/service/OptusService.java
+++ app/src/main/java/com/cocosw/optus/service/OptusService.java
@@ -17,6 +17,11 @@ public class OptusService extends Network {
         return Observable.fromCallable(new Callable<Route[]>() {
             @Override
             public Route[] call() throws Exception {
+                // MOCK
+//                Thread.sleep(5000);
+//                throw new CocoException("error");
+//                return new Gson().fromJson(new InputStreamReader(CocoApp.getApp().getAssets().open("sample.json")),Route[].class);
+//                return new Route[]{};
                 return Network.request("http://express-it.optusnet.com.au/sample.json", Route[].class);
             }
         });
